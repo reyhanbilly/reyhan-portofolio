11b4cfb8b15bf106d9ece802ffc7e129660ff31c
reyhanbilly <raihanzanzabili@gmail.com>
2026-01-30 17:26:49 +0700

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Portofolio/",
+  base: "/reyhan-portofolio/",
 })
